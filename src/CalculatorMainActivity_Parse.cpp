b98dafc586753e76d2f4b3af649b77d0e78617f4
thediscorddev <paypalchplay@gmail.com>
2024-10-26 13:25:24 +0700

    a

--- src/CalculatorMainActivity_Parse.cpp
+++ src/CalculatorMainActivity_Parse.cpp
@@ -8,8 +8,7 @@ double CalculatorMainActivity::Calculate(int index)
 {
     //Parser
     float CurrentNumPart = 0;
-    int decimalNum = 1;
-    bool IsDecimalPart = false;
+    int decimalNum = 0;
     int bracketLevel = 0;
     int Eid = 0;
     std::map<std::string,std::vector<std::string>> ValueList; //important
@@ -22,14 +21,14 @@ double CalculatorMainActivity::Calculate(int index)
         {
             std::cout << a << std::endl;
             std::cout << i << std::endl;
-            if(IsDecimalPart == false) CurrentNumPart=CurrentNumPart*10+std::stoi(a);
+            if(decimalNum == 0) CurrentNumPart=CurrentNumPart*10+std::stoi(a);
             else {
                 int m = 1;
                 for(int i = 0; i < decimalNum; i++)
                 {
                     m*=10;
                 }
-                CurrentNumPart+=std::stoi(a)/m;
+                CurrentNumPart+=(float) std::stoi(a)/m;
                 decimalNum++;
             }
         }
@@ -37,63 +36,61 @@ double CalculatorMainActivity::Calculate(int index)
         {
             // Not a number, possibly "*", "-", "+", "\", "sin("
             if(a == ".") {
-                IsDecimalPart = true;
+                decimalNum = 1;
             }
             if(a == "+" || a == "-" || a == "*" || a == "/") {
                 std::string o = a;
                 PlannedOperation.push_back(std::to_string(CurrentNumPart));
                 PlannedOperation.push_back(o);
                 CurrentNumPart = 0;
-                decimalNum = 1;
-                IsDecimalPart = false;
+                decimalNum = 0;
             }
         }
     }
     PlannedOperation.push_back(std::to_string(CurrentNumPart));
     CurrentNumPart = 0;
-    decimalNum = 1;
-    IsDecimalPart = false;
-    int mdCount = 1;
-    while(mdCount != 0)
+    decimalNum = 0;
+    int ia = 0;
+    bool mdStillAvailable = true;
+    while(mdStillAvailable == true)
     {
-        mdCount = 0;
-        for(int i = 0; i < PlannedOperation.size(); i++)
+        mdStillAvailable = false;
+        if(ia >= PlannedOperation.size()) ia = 0;
+        const auto& a = PlannedOperation.at(ia);
+        if(a == "*" || a == "/")
         {
-            const auto& a = PlannedOperation.at(i);
-            if(a == "*" || a == "/")
+            if(ia -1 >= 0 && ia + 1 <= PlannedOperation.size())
             {
-                mdCount++;
-                if(i - 1 >= 0 && i + 1 <= PlannedOperation.size())
+                try
                 {
-                    try
+                    /* code */
+                    std::string na = PlannedOperation.at(ia-1);
+                    std::string nb = PlannedOperation.at(ia+1);
+                    float fna = std::stof(na);
+                    float fnb = std::stof(nb);
+                    float resulta = 0;
+                    if(a == "*")
                     {
-                        /* code */
-                        std::string na = PlannedOperation.at(i-1);
-                        std::string nb = PlannedOperation.at(i+1);
-                        float fna = std::stof(na);
-                        float fnb = std::stof(nb);
-                        float resulta = 0;
-                        if(a == "/" && fnb == 0) {
-                            resulta = 999999;
-                        }else if(a == "/" && fnb != 0)
-                        {
-                            resulta = fna/fnb;
-                        }else if (a == "*") resulta = fna*fnb;
-                        PlannedOperation.erase(PlannedOperation.begin() + i -1,PlannedOperation.begin() + i + 2);
-                        PlannedOperation.insert(PlannedOperation.begin() + i - 1,std::to_string(resulta));
-                    }
-                    catch(const std::exception& e)
-                    {
-                        std::cerr << e.what() << '\n';
-                    }
-                    
-                }else {
-
+                        resulta = fna*fnb;
+                    }else if (a == "/" && fnb != 0) resulta = fna/fnb;
+                    else if (a == "/" && fnb == 0) resulta = 999999;
+                    PlannedOperation.erase(PlannedOperation.begin() + ia -1,PlannedOperation.begin() + ia + 2);
+                    PlannedOperation.insert(PlannedOperation.begin() + ia - 1,std::to_string(resulta));
+                    ia = 0;
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << '\n';
                 }
             }
         }
+        ia++;
+        for(const auto& a: PlannedOperation)
+        {
+            if(a == "*" || a == "/") mdStillAvailable = true;
+        }
     }
-    int ia = 0;
+    ia = 0;
     while(PlannedOperation.size() > 1)
     {
         if(ia >= PlannedOperation.size()) ia = 0;
